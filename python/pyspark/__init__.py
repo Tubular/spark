eec849aa8a61ab9a69a08547e468bb7c1ee98763
Dmytro Popovych <drudim.ua@gmail.com>
2017-07-13 11:04:05 +0300

    IMP add setup.py for pyspark
    Make PySpark friendly to the Python ecosystem.
    Now, we consider it as a package with dependencies on py4j.
    Changes were tested manually.

--- python/pyspark/__init__.py
+++ python/pyspark/__init__.py
@@ -36,6 +36,43 @@ Public classes:
       Finer-grained cache persistence levels.
 
 """
+import logging
+import os
+import re
+
+
+logger = logging.getLogger(__name__)
+
+
+def get_spark_version():
+    """
+    Detect version of Spark located under SPARK_HOME.
+    """
+    spark_home = os.environ.get('SPARK_HOME')
+    if not spark_home:
+        logger.warning("Environment variable SPARK_HOME is not set")
+        return
+
+    libs_path = os.path.join(os.environ['SPARK_HOME'], 'jars')
+    for jar_file in os.listdir(libs_path):
+        if jar_file.startswith('spark-core'):
+            try:
+                return re.match('spark-core_[\d\.]+-([\d\.]+)\.jar', jar_file).group(1)
+            except IndexError:
+                logger.warning("Can't detect Spark version by the spark-core jar file")
+
+    logger.warning("Can't find file with Spark version")
+
+
+installed_version = get_spark_version()
+__version__ = '2.1.1'
+
+
+if installed_version is None:
+    logger.warning("Spark is not installed on this machine")
+elif installed_version != __version__:
+    logger.warning("Incompatible versions of PySpark (%s) and Spark (%s)",
+                   __version__, installed_version)
 
 from functools import wraps
 import types
